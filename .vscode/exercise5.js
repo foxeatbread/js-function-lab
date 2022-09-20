8af8ca2ab2a18bc60045c369f0c892d53d36b173
Gu Haoxuan <985121841@qq.com>
2022-09-20 12:07:49 +0100

    Exercise5.

--- .vscode/exercise5.js
+++ .vscode/exercise5.js
@@ -2,8 +2,13 @@ fetch("https://jsonplaceholder.typicode.com/todos")
   .then(response => response.json())
   .then(responseBody => {
      const todos = responseBody;
-     const uncompletedTodos = todos.filter(todos => todos.completed==false).map(todo => {return {title: `${todo.userID}-${todo.title} `};});
-     console.log(uncompletedTodos)
+     const answer = todos.reduce((acc,todo) => {
+        todo.completed
+        ?acc
+        :acc.push({userId:todo.userId,title:todo.title})
+        return acc;
+     },[])
+     console.log(answer)
   })
   .catch(function(err) { 
     console.log(err);
