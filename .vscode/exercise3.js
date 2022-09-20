f83b4cd58b68d254120d53878b570faf64cb4d08
Gu Haoxuan <985121841@qq.com>
2022-09-20 11:48:36 +0100

    Exercise 4.

--- .vscode/exercise3.js
+++ .vscode/exercise3.js
@@ -2,7 +2,7 @@ fetch("https://jsonplaceholder.typicode.com/todos")
   .then(response => response.json())
   .then(responseBody => {
      const todos = responseBody;
-     const uncompletedTodos = todos.filter(todos => todos.completed==true).map(todo => {return {title: `${todo.userID}-${todo.title} `,};});
+     const uncompletedTodos = todos.filter(todos => todos.completed==false).map(todo => {return {title: `${todo.userID}-${todo.title} `,};});
      console.log(uncompletedTodos)
   })
   .catch(function(err) { 
--- .vscode/exercise4.js
+++ .vscode/exercise4.js
@@ -2,6 +2,9 @@ fetch("https://jsonplaceholder.typicode.com/todos")
   .then(response => response.json())
   .then(responseBody => {
      const todos = responseBody;
+     const completedID = todos.filter(todos => todos.completed==true).map(todo => `${todo.userID}`);
+     const completedTodoNum = completedID.reduce((initialValue) =>{return initialValue+1},0)
+     console.log(completedTodoNum)
   })
   .catch(function(err) { 
     console.log(err);
