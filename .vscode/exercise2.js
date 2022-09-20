52e53180c606d694bfa99754516ef925fa724024
Gu Haoxuan <985121841@qq.com>
2022-09-20 11:20:41 +0100

    Exercise 2.

--- .vscode/exercise2.js
+++ .vscode/exercise2.js
@@ -1,11 +1,15 @@
 fetch("https://jsonplaceholder.typicode.com/todos")
   .then(response => response.json())
   .then(responseBody => {
-     const profiles = responseBody;
-    //  const completedTodo = profiles.filter(profiles => profiles.completed==true);
-     profiles.forEach( todo => {
-      console.log(`${todo.title}`)
-    });
+     const todos = responseBody;
+     const todoTitles = todos.map( todo => {
+        return {
+            title: `${todo.title} `,
+        };
+      });
+      todoTitles.forEach(titles => {
+        console.log(`${titles.title}`)
+      });
   })
   .catch(function(err) { 
     console.log(err);
